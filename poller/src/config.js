1e531232528d28dc2856957e27f8495150a1860d
rbalak <bala.rishikesavan@gmail.com>
2016-08-02 14:16:17 +0530

    updated mininet, odl and influxDB host

--- poller/src/config.js
+++ poller/src/config.js
@@ -8,13 +8,13 @@ function define(name, value) {
 define("txTraffic","[MN=TransmittedBytes]");
 define("rxTraffic","[MN=ReceivedBytes]");
 define("portStats","[DC=PORTSTATS]");
-define("odlHost", "54.254.188.41");
+define("odlHost", "54.254.158.212");
 define("odlPort",8181);
 define("path","/tsdr/metrics/query?tsdrkey=");
 define("pollInterval",60000);
 define("pollHistory",120);
-define("influxDBHost", "192.168.231.104");
-define("influxDBPort",18086);
+define("influxDBHost", "http://influxdb-admin-sdnorchestrator.apps.infosys.openshift3roadshow.com");
+define("influxDBPort",8080);
 define("influxDBwritePath","/write");
 define("pmStatsDB","pmstats");
 define("influxDReadPath", "/query?pretty=true");
@@ -23,7 +23,7 @@ define("metricCalulationInterval", 120000);
 define("metricCalculationHistory",240);
 define("txSeries", "TransmittedBytes");
 define("rxSeries", "ReceivedBytes");
-define("mininetHost", "54.254.188.41");
+define("mininetHost", "54.254.158.212");
 define("mininetPort", "8182");
 define("mininetBwPath", "/linkbandwidth/s1-s2");
 
